4bb14e8548e9abc6c2b8908f662e83f3c75e64bf
Jubílio Maússe <jubiliomausse5@gmail.com>
2026-08-04 12:17:45 +0200

    Correct RSS feed URLs

--- generate_rss.py
+++ generate_rss.py
@@ -1,27 +1,43 @@
-import os
 from feedgen.feed import FeedGenerator
+
+
+SITE_URL = "https://jubilio.github.io/cv_articles"
+BLOG_URL = `${SITE_URL}/pages/blog`
+ARTICLE_URL = `${SITE_URL}/blog/gee-banhine-lulc`
+
 
 def generate_feeds():
     print("Generating RSS and Atom feeds for the blog...")
-    fg = FeedGenerator()
-    fg.id('https://jubiliomausse.github.io/blog')
-    fg.title('Jubílio Maússe - Blog')
-    fg.author({'name': 'Jubílio Filiano Maússe', 'email': 'jubiliomausse5@gmail.com'})
-    fg.link(href='https://jubiliomausse.github.io', rel='alternate')
-    fg.description('Artigos, Dicas SIG e Reflexões sobre Ajuda Humanitária')
 
-    # Example of adding one static entry to ensure the feed isn't empty. 
-    # In a fully robust system, this would parse markdown frontmatter from the blog/ folder.
+    fg = FeedGenerator()
+    fg.id(BLOG_URL)
+    fg.title("Jubílio Maússe - Blog")
+    fg.author(
+        {
+            "name": "Jubílio Filiano Maússe",
+            "email": "jubiliomausse5@gmail.com",
+        }
+    )
+    fg.link(href=BLOG_URL, rel="alternate")
+    fg.description(
+        "Artigos, dicas SIG e reflexões sobre análise geoespacial "
+        "e acção humanitária"
+    )
+    fg.language("pt")
+
     fe = fg.add_entry()
-    fe.id('https://jubiliomausse.github.io/blog/primeiro-post')
-    fe.title('Primeiro Post do Blog: GIS e Acção Humanitária')
-    fe.link(href='https://jubiliomausse.github.io/blog/primeiro-post.html')
-    fe.description('A inaugurar o blog com notas sobre mapas e RRM.')
-    
-    # Write to files
-    fg.rss_file('rss.xml')
-    fg.atom_file('atom.xml')
+    fe.id(ARTICLE_URL)
+    fe.title("Classificação LULC com GEE: Parque Nacional do Banhine")
+    fe.link(href=ARTICLE_URL)
+    fe.description(
+        "Guia prático sobre Google Earth Engine e classificação "
+        "de uso e cobertura da terra no Parque Nacional do Banhine."
+    )
+
+    fg.rss_file("rss.xml")
+    fg.atom_file("atom.xml")
     print("Successfully generated rss.xml and atom.xml")
+
 
 if __name__ == "__main__":
     generate_feeds()
